c9506e82302f63fa69b26d619aab717e2f4260d8
emmyn5600 <emmy6618@gmail.com>
2021-07-05 17:37:25 +0200

    modify webpack config with the babel-loader to run our code through the babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,20 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'app.bundle.js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src/'),
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ]
   }
 }
