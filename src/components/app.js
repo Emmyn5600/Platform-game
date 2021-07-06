4e666b827dfc4d80e691059083eeb7cd714a23e6
emmyn5600 <emmy6618@gmail.com>
2021-07-06 16:15:08 +0200

    Add app.js file

---
+++ src/components/app.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import api from '../helpers_api/api';
+import { leaderboardData } from '../helpers_api/data';
+
+
+export const saveUser = (e) => {
+  e.preventDefault();
+  localStorage.setItem('endless_flying_current_user', new FormData(e.target).get('username'));
+  if (leaderboardData.get() != null) { switchComponents('options'); }
+  return true;
+};
+
+export const getCurrentUser = () => localStorage.getItem('endless_flying_current_user');
+
+export const loadLeaderboard = async () => {
+  const { data: { result } } = await axios.get(api);
+  return result.sort((a, b) => b.score - a.score);
+};
+
+export const saveScore = async (obj) => {
+  const { data } = await axios.post(api, obj);
+  return data;
+};
