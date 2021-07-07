b5788a33084a9986cf963506c2a55af8b4c088a0
emmyn5600 <emmy6618@gmail.com>
2021-07-07 11:00:10 +0200

    Add logic.test.js file for testing

---
+++ logic.test.js
@@ -0,0 +1,26 @@
+import { getCurrentUser, saveUser, loadLeaderboard } from './src/components/app';
+
+
+test('Save a user', () => {
+  const e = { preventDefault: () => {} };
+  const res = saveUser(e);
+  expect(res).toBeDefined();
+  expect(typeof res).toBe('boolean');
+  expect(res).toBe(true);
+});
+
+test('Currentuser', () => {
+  const user = getCurrentUser();
+  expect(user).toBe('null');
+});
+
+test('Load leaderboard', async () => {
+  const res = await loadLeaderboard();
+
+  expect(typeof res).toBe('object');
+  expect(res[0]).toBeDefined();
+  expect(res[0].user).toBeDefined();
+  expect(res[0].score).toBeDefined();
+  expect(typeof res[0].user).toBe('string');
+  expect(typeof res[0].score).toBe('number');
+});
